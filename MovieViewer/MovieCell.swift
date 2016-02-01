247177f4cc84a6e1e72fb1de75551f7dac4a88a6
navatar5 <librarylaptop@Librarys-MacBook-Pro.local>
2016-02-01 18:48:25 -0500

    MovieViewer Project - Assignment 1

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by  on 1/31/16.
+//  Copyright © 2016 Pranesh Navarathna. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var TitleLabel: UILabel!
+
+    @IBOutlet weak var OverviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
